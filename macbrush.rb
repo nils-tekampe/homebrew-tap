70a391237745dc0f5e1c1b3bd004aaff17e659ca
Nils Tekampe <nils@tekampe.org>
2016-02-19 13:43:32 +0100

    Update macbrush.rb

--- macbrush.rb
+++ macbrush.rb
@@ -1,7 +1,7 @@
 class Macbrush < Formula
   desc "Keeping OSX directories clear of temporary files"
   homepage "https://nils-tekampe.github.io/macbrush"
-  url "https://github.com/nils-tekampe/macbrush/archive/0.8.tar.gz"
+  url "https://github.com/nils-tekampe/macbrush/archive/0.81.tar.gz"
   sha256 "3c17d2bd0c985aa2c608fc1a3df7bcb3348115aac02d15a88de80e53c8782820"
 
   depends_on :xcode => :build
