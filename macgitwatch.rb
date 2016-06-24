f02ac0b9f8300ace0e877cdc0dec9533c8ec06ba
Nils Tekampe <n.tekampe@tuvit.de>
2016-06-24 22:08:49 +0200

    working

--- macgitwatch.rb
+++ macgitwatch.rb
@@ -1,21 +1,14 @@
-class Macbrush < Formula
-  desc "Keeping OSX directories clear of temporary files"
-  homepage "https://nils-tekampe.github.io/macbrush"
+class macgitwatch < Formula
+  desc "Auto commit for git under OSX"
+  homepage "https://github.com/nils-tekampe/macgitwatch"
   url "https://github.com/nils-tekampe/macbrush/archive/0.81.tar.gz"
   sha256 "c36ac25eb898868898991671d42efb4fb1c56895e3161519ffbd70ea20ea09ef"
 
-  depends_on :xcode => :build
+  depends_on "git"
+  depends_on "fswatch"
 
   def install
-    cd "macbrush"
-    xcodebuild  "clean"
-    xcodebuild 
     bin.install "./build/Release/macbrush"
-    man1.install "./macbrush/macbrush.1"
   end
     
-  test do
-  system macbrush, "--verbose --skip-observation", testpath
-  end
-  
 end
