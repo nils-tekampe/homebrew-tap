7f5224a50d363a51167b6d3760d220139bb6b6b0
Nils Tekampe <n.tekampe@tuvit.de>
2016-06-25 11:01:22 +0200

    new version for macgitwatch

--- macgitwatch.rb
+++ macgitwatch.rb
@@ -2,7 +2,7 @@ class Macgitwatch < Formula
   desc "Auto commit for git under OSX"
   homepage "https://github.com/nils-tekampe/macgitwatch"
   url "https://github.com/nils-tekampe/macgitwatch/archive/0.1.tar.gz"
-  sha256 "800a44623eaf4a19db3942d03acb34130df87efb6e5ad1bf79cd3a76b5f4f22c"
+  sha256 "800a44623eaf4a19db3942d03acb34130df87efb6e5ad1bf79cd3a76b5f4f22o"
 
   depends_on "git"
   depends_on "fswatch"
