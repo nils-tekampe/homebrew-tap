9be31bf1ed61246be5eff6902d4b593019b13949
Nils Tekampe <nils@tekampe.org>
2016-12-26 20:50:23 +0100

    Create xmpl.rb

---
+++ xmpl.rb
@@ -0,0 +1,12 @@
+class xmpl < Formula
+  desc "xmpl - commandline by example"
+  homepage "https://github.com/nils-tekampe/xmpl"
+  url "https://github.com/nils-tekampe/xmpl/archive/0.0.1.tar.gz"
+  sha256 "7d4b6299093cdd2d88bfe08f9da06b47756d98f69e2daab1f716b943726d6ec5"
+
+  def install
+    bin.install "./xmpl"
+    bin.install "./xmpls/ls.7"
+  end
+    
+end
