3a51053120c152c0f5d4b14b45328c933d45e5fe
Nils Tekampe <nils@tekampe.org>
2016-12-26 20:51:36 +0100

    Update xmpl.rb

--- xmpl.rb
+++ xmpl.rb
@@ -1,4 +1,4 @@
-class xmpl < Formula
+class Xmpl < Formula
   desc "xmpl - commandline by example"
   homepage "https://github.com/nils-tekampe/xmpl"
   url "https://github.com/nils-tekampe/xmpl/archive/0.0.1.tar.gz"
