aa1657c6f83d32f46a8959f2ab1d9ec4ef4df398
Nils Tekampe <nils@tekampe.org>
2016-12-26 20:52:42 +0100

    Update xmpl.rb

--- xmpl.rb
+++ xmpl.rb
@@ -2,7 +2,7 @@ class Xmpl < Formula
   desc "xmpl - commandline by example"
   homepage "https://github.com/nils-tekampe/xmpl"
   url "https://github.com/nils-tekampe/xmpl/archive/0.0.1.tar.gz"
-  sha256 "7d4b6299093cdd2d88bfe08f9da06b47756d98f69e2daab1f716b943726d6ec5"
+  sha256 "cd4f59687aa0dc5f45ec548da1a7be9a728e4af9bf5da027041d0f9211b3e506"
 
   def install
     bin.install "./xmpl"
