01844cc5ad4acc027faee6bb3c1f8e67a79c7a46
Nils Tekampe <nils@tekampe.org>
2019-04-07 19:27:23 +0200

    Update htmldiff.rb

--- htmldiff.rb
+++ htmldiff.rb
@@ -1,1 +1,11 @@
+class Htmldiff < Formula
+  desc "A diff tool to diff two html files"
+  homepage "https://github.com/nils-tekampe/htmldiff"
+  url "https://github.com/nils-tekampe/macgitwatch/archive/0.4.tar.gz"
+  sha256 "7d4b6299093cdd2d88bfe08f9da06b47756d98f69e2daab1f716b943726d6ec5"
 
+  def install
+    bin.install "./macgitwatch.sh"
+  end
+    
+end
