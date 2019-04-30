5a02d15386a1d36ce3a0dadb6b3953905bc3b18e
Nils Tekampe <nils@tekampe.org>
2019-04-30 18:02:23 +0200

    Update htmldiff.rb

--- htmldiff.rb
+++ htmldiff.rb
@@ -1,11 +1,11 @@
-class Htmldiff < Formula
+class htmldiff < Formula
   desc "A diff tool to diff two html files"
   homepage "https://github.com/nils-tekampe/htmldiff"
   url "https://github.com/nils-tekampe/htmldiff/archive/v1.0.0.tar.gz"
   sha256 "021aa7ec5ff11bfc142c0b69f4f95766d85c3d4d5b35583b6b3e94c987b4f791"
 
   def install
-    mv "bin/htmldiff-macos", "bin/htmldiff"
+    mv "htmldiff-macos", "htmldiff"
     bin.install "./htmldiff"
     
   end
